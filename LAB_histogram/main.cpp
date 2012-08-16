5fb8bdf82c3677b077e5e3e6fa25ce5573089098
zippon <zippon.wu@gmail.com>
2012-08-16 13:42:49 +0900

    Initial Commit

---
+++ LAB_histogram/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LAB_histogram
+//
+//  Created by Zhipeng Wu on 8/16/12.
+//  Copyright (c) 2012 Zhipeng Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
